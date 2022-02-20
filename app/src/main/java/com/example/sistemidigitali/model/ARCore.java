6c85917453750a1ccd34504b3d3e65a62edf77ec
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-02-20 13:10:38 +0100

    Update ARCore.java

--- app/src/main/java/com/example/sistemidigitali/model/ARCore.java
+++ app/src/main/java/com/example/sistemidigitali/model/ARCore.java
@@ -2,10 +2,14 @@ package com.example.sistemidigitali.model;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.Image;
 import android.util.Log;
 
 import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.exceptions.UnavailableException;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class ARCore {
 
@@ -15,14 +19,14 @@ public class ARCore {
         this.context = context;
     }
 
-    // Verify that ARCore is installed and using the current version.
+    //Verify that ARCore is installed and using the current version
     private boolean isARCoreSupportedAndUpToDate() {
         ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this.context);
         switch (availability) {
             case SUPPORTED_INSTALLED:
                 return true;
-
             case SUPPORTED_APK_TOO_OLD:
+                return false;
             case SUPPORTED_NOT_INSTALLED:
                 try {
                     // Request ARCore installation or update if needed.
@@ -38,19 +42,20 @@ public class ARCore {
                     Log.e("ARCORE", "ARCore not installed", e);
                 }
                 return false;
-
             case UNSUPPORTED_DEVICE_NOT_CAPABLE:
                 // This device is not supported for AR.
                 return false;
-
-            case UNKNOWN_CHECKING:
-                // ARCore is checking the availability with a remote query.
-                // This function should be called again after waiting 200 ms to determine the query result.
-            case UNKNOWN_ERROR:
-            case UNKNOWN_TIMED_OUT:
-                // There was an error checking for AR availability. This may be due to the device being offline.
-                // Handle the error appropriately.
         }
         return true;
+    }
+
+    //Obtain the depth in millimeters for depthImage at coordinates (x, y)
+    public int getMillimetersDepth(Image depthImage, int x, int y) {
+        // The depth image has a single plane, which stores depth for each
+        // pixel as 16-bit unsigned integers.
+        Image.Plane plane = depthImage.getPlanes()[0];
+        int byteIndex = x * plane.getPixelStride() + y * plane.getRowStride();
+        ByteBuffer buffer = plane.getBuffer().order(ByteOrder.nativeOrder());
+        return buffer.getShort(byteIndex);
     }
 }
